c4cf8d8b3a8c92e08ff4ff6184e67c2cd6288b75
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2024-01-02 15:31:54 +0600

    actionTypes

--- redux-starter/src/index.js
+++ redux-starter/src/index.js
@@ -1,11 +1,20 @@
 import store from './store'
 
-store.subscribe(() => {
+const unsubscribe = store.subscribe(() => {
   console.log('Store changed!', store.getState())
 })
 
 store.dispatch({
   type: 'bugAdded',
+  payload: {
+    description: 'Bug1',
+  },
+}) // { counter: 0 }
+
+unsubscribe()
+
+store.dispatch({
+  type: 'bugRemoved',
   payload: {
     description: 'Bug1',
   },
