0d4b47ef548f21af6594059a645f56d6c0ff6568
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2024-01-03 23:49:30 +0600

    FetchQuery

--- basics/src/App.jsx
+++ basics/src/App.jsx
@@ -2,14 +2,9 @@ import FetchSwr from './components/SWR/FetchSwr'
 
 const App = () => {
   return (
-    <suspense
-      fallback={
-        <div>
-          <h1>Loading...</h1>
-        </div>
-      }>
+    <div>
       <FetchSwr />
-    </suspense>
+    </div>
   )
 }
 
---
+++ basics/src/components/FetchQuery/FetchQuery.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const FetchQuery = () => {
+  return <div>FetchQuery</div>;
+};
+
+export default FetchQuery;
