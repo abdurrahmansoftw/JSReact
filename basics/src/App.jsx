60a30cee24652a3c9364b2c3eed436b31364ff22
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2024-01-04 13:34:41 +0600

    Suspense

--- basics/src/App.jsx
+++ basics/src/App.jsx
@@ -1,12 +1,20 @@
+import { Suspense } from 'react'
 import { QueryClient, QueryClientProvider } from 'react-query'
 import FetchQuery from './components/FetchQuery/FetchQuery'
 
 const App = () => {
-  const queryClientt = new QueryClient()
+  const queryClientt = new QueryClient({
+    defaultOptions: {
+      queries: {
+        suspense: true,
+      },
+    },
+  })
   return (
     <QueryClientProvider client={queryClientt}>
-      <FetchQuery />
-      <FetchQuery />
+      <Suspense fallback={<div>Loading...</div>}>
+        <FetchQuery />
+      </Suspense>
     </QueryClientProvider>
   )
 }
