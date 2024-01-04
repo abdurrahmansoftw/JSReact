93022b40f4a00ac5359ce34e805c1f1d9657f841
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2024-01-04 20:52:59 +0600

    dynamicCounterReducer

--- redux/src/redux/dynamicCounter/counterReducer.js
+++ redux/src/redux/dynamicCounter/counterReducer.js
@@ -7,9 +7,9 @@ const initialState = {
 const dynamicCounterReducer = (state = initialState, action) => {
   switch (action.type) {
     case DINCREMENT:
-      return { ...state, value: state.value + 1 }
+      return { ...state, value: state.value + action.payload.value }
     case DDECREMENT:
-      return { ...state, value: state.value - 1 }
+      return { ...state, value: state.value - action.payload.value }
     case DRESET:
       return { ...state, value: 0 }
     default:
