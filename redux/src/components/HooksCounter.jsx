de4407c658184646d2b095cb62a0cb03f5cfd2d5
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2024-01-04 17:27:30 +0600

    useDispatch

--- redux/src/components/HooksCounter.jsx
+++ redux/src/components/HooksCounter.jsx
@@ -1,11 +1,16 @@
 import { Box, Paper } from '@mui/material'
-import { useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import Buttons from './Buttons'
 import Count from './Count'
 
 // eslint-disable-next-line react/prop-types
 const HooksCounter = ({ id }) => {
   const count = useSelector((state) => state.value)
+  const dispatch = useDispatch()
+
+  const increment = (value) => dispatch(increment(value))
+  const decrement = (value) => dispatch(decrement(value))
+  const reset = () => dispatch(reset())
   return (
     <Box>
       <Paper sx={{ p: 2, my: 5 }} elevation={2}>
