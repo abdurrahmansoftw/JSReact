a27bbdfb4427978c36deeb066225b9805fd4329e
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2024-01-04 16:12:33 +0600

    actions type

--- redux/src/redux/actions.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function actions() {
-  return <div>actions</div>;
-}
-
-export default actions;
--- redux/src/redux/reducers.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const reducers = () => {
-  return <div>reducers</div>;
-};
-
-export default reducers;
