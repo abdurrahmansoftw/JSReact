cefa736fab62d54ff79bfcd9f51a54a9b5b625bf
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2024-01-04 16:17:25 +0600

    counterReducer

---
+++ redux/src/redux/counter/counterReducer.js
@@ -0,0 +1,16 @@
+const initialState = {
+  value: 0,
+}
+
+const counterReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case 'INCREMENT':
+      return { ...state, value: state.value + action.payload }
+    case 'DECREMENT':
+      return { ...state, value: state.value - action.payload }
+    case 'RESET':
+      return { ...state, value: 0 }
+    default:
+      return state
+  }
+}
