74a3d375704a45a3a3f0ef705591c215c9cae4d5
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2024-01-04 17:31:59 +0600

    Hander

--- redux/src/components/HooksCounter.jsx
+++ redux/src/components/HooksCounter.jsx
@@ -1,5 +1,6 @@
 import { Box, Paper } from '@mui/material'
 import { useDispatch, useSelector } from 'react-redux'
+import { decrement, increment, reset } from '../redux/counter/actions'
 import Buttons from './Buttons'
 import Count from './Count'
 
@@ -8,17 +9,24 @@ const HooksCounter = ({ id }) => {
   const count = useSelector((state) => state.value)
   const dispatch = useDispatch()
 
-  const increment = (value) => dispatch(increment(value))
-  const decrement = (value) => dispatch(decrement(value))
-  const reset = () => dispatch(reset())
+  const incrementHander = (value) => {
+    dispatch(increment(value))
+  }
+  const decrementHander = (value) => {
+    dispatch(decrement(value))
+  }
+  const resetHander = () => {
+    dispatch(reset())
+  }
+
   return (
     <Box>
       <Paper sx={{ p: 2, my: 5 }} elevation={2}>
         <Count count={count} id={id} />
         <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
-          <Buttons handler={increment}> + </Buttons>
-          <Buttons handler={reset}> Reset </Buttons>
-          <Buttons handler={decrement}> - </Buttons>
+          <Buttons handler={() => incrementHander(2)}> + </Buttons>
+          <Buttons handler={() => resetHander(0)}> Reset </Buttons>
+          <Buttons handler={() => decrementHander(2)}> - </Buttons>
         </Box>
       </Paper>
     </Box>
