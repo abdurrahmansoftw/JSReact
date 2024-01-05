b2aa30a39d18b11d3010a82b9c1e8dca93671063
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2024-01-05 21:57:07 +0600

    upCommingState

--- redux/src/redux/store.js
+++ redux/src/redux/store.js
@@ -4,6 +4,9 @@ import rootReducer from './rootReducer'
 const myLogger = (store) => (next) => (action) => {
   console.log('Logged Action: ', JSON.stringify(action))
   console.log('State: ', JSON.stringify(store.getState()))
+
+  const upCommingState = [action].reduce(rootReducer, store.getState())
+
   return next(action)
 }
 
