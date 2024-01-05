0a91817a82edb4dd0cd2f82882bd7fc7e85e77bd
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2024-01-05 21:55:11 +0600

    myLogger

--- redux/src/redux/store.js
+++ redux/src/redux/store.js
@@ -2,7 +2,8 @@ import { applyMiddleware, createStore } from 'redux'
 import rootReducer from './rootReducer'
 
 const myLogger = (store) => (next) => (action) => {
-  console.log('Logged Action: ', action)
+  console.log('Logged Action: ', JSON.stringify(action))
+  console.log('State: ', JSON.stringify(store.getState()))
   next(action)
 }
 
