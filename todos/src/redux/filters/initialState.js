6ad03ba5097d4784815c2ab4edfeb64eea101212
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2024-01-06 15:29:23 +0600

    Add Toggle

---
+++ todos/src/redux/filters/initialState.js
@@ -0,0 +1,6 @@
+export const initialState = {
+  filters: {
+    status: null,
+    colors: [],
+  },
+}
---
+++ todos/src/redux/todos/reducer.js
@@ -0,0 +1,27 @@
+import { ADD_TODO, COLOR_SELECTED, TOGGLE_TODO } from './actionTypes'
+import { initialState } from './initialState'
+
+function nextTodoId(todos) {
+  const maxId = todos.reduce((maxId, todo) => Math.max(todo.id, maxId), -1)
+  return maxId + 1
+}
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case ADD_TODO:
+      return [...state, { id: nextTodoId(state) }]
+
+    case TOGGLE_TODO:
+      return state.map((todo) =>
+        todo.id === action.payload.id
+          ? { ...todo, completed: !todo.completed }
+          : todo
+      )
+
+
+    default:
+      return state
+  }
+}
+
+export default reducer
