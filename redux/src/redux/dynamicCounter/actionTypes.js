c521c77c91d21f84245a5f53aee1e2d2202007f6
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2024-01-22 09:43:50 +0600

    paypal getways

--- redux/src/redux/dynamicCounter/actionTypes.js
+++
@@ -1,3 +0,0 @@
-export const DINCREMENT = 'dynamicCounter/increment'
-export const DDECREMENT = 'dynamicCounter/decrement'
-export const DRESET = 'dynamicCounter/reset'
--- redux/src/redux/store.js
+++
@@ -1,8 +0,0 @@
-import { applyMiddleware, createStore } from 'redux'
-import logger from 'redux-logger'
-import myLogger from './middlewares/myLogger'
-import rootReducer from './rootReducer'
-
-const store = createStore(rootReducer, applyMiddleware(logger, myLogger))
-
-export default store
